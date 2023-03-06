19bce87bf48c7bda1aef557d8b6b161f6b4c628f
Denise Recofka <denise@Robinsons-MBP.home>
2023-03-06 13:57:39 +0100

    Buttons styling + readme

--- src/theme/theme.js
+++ src/theme/theme.js
@@ -5,7 +5,6 @@ const Theme = extendTheme({
   styles: {
     global: (props) => ({
       body: {
-        // fontFamily: 'body',
         bg: mode('lightviolet', 'spacecadet')(props)
       }
     })
@@ -13,22 +12,19 @@ const Theme = extendTheme({
   colors: {
     violet: '#9c77d7',
     lightviolet: '#845EC2',
-    sunglow: '#ffc857',
-    darkcyan: '#119da4',
-    verylightcyan: '#ADC5CF',
-    paynesgray: '#19647eff',
-
+    lightcyan: '#ADC5CF',
+    turquoise: '#119DA4',
+    yellow: '#FFC857',
     spacecadet: '#1f2041',
     primary100: '#1f2041',
-    primary200: '#353554',
     primary300: '#4c4b68',
     primary400: '#64627c',
     primary500: '#7c7a90',
     primary600: '#9593a6'
+  },
+  fonts: {
+    heading: `'Kanit', sans-serif`,
+    body: `'Rubik', sans-serif`
   }
 });
 export default Theme;
-
-// CSS DARK THEME PRIMARY COLORS
-
-// CSS DARK THEME SURFACE COLORS
